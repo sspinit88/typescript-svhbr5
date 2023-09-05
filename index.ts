ff0b01b4c0dbbba1896b9ec4af4c84bfd9a7b7eb
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-09-05 16:54:44 +0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,38 @@
 // Import stylesheets
 import './style.css';
+
+//// Utility ////
+
+interface User {
+  name: string;
+  age: number;
+}
+
+interface UserData {
+  phone?: number;
+  addres?: string;
+}
+
+/* ! Readonly - закрывает поля для редактирования */
+
+const user: Readonly<User> = {
+  name: 'Alex',
+  age: 65,
+};
+
+user.name = 'Tom'; /// Cannot assign to 'name' because it is a read-only property.
+
+/* ! Required - все поля делает обязательными к заполнению */
+
+const userData: Required<UserData> = {
+  phone: 132,
+  addres: 'Street',
+};
+
+/* Record -  */
+
+interface PageInfo {
+  title: string;
+}
+
+type Route = 'home' | 'about' | 'contact';
