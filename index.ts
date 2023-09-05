93fae8cc62234aaa242278290f86762f83313245
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2023-09-05 17:23:08 +0400

    1 file modified

--- index.ts
+++ index.ts
@@ -3,8 +3,83 @@ import './style.css';
 
 //// Utility ////
 
-/* ! 
-     Readonly<T> - делает все свойства объекта типа T доступными только для чтения. 
+/* 
+  Awaited<T> - это специальный тип, который может быть использован для обозначения типа, 
+  который будет возвращен из асинхронной функции. 
+*/
+
+async function getData(): Promise<string> {
+  return 'hello';
+}
+
+let awaitedData: Awaited<ReturnType<typeof getData>>;
+// теперь awaitedData может быть 'hello'
+
+/* 
+  Exclude<UnionType, ExcludedMembers> - исключает определенные типы из объединенного типа.
+*/
+
+type A = 'a' | 'b' | 'c';
+type B = Exclude<A, 'a' | 'b'>;
+// теперь B это 'c'
+
+const t: B = 'c';
+
+/*
+  Extract<Type, Union> - извлекает из типа Type только те типы, которые присутствуют в Union.
+*/
+
+type A2 = 'a' | 'b' | 'c';
+type B2 = 'a' | 'b';
+type C2 = Extract<A, B>;
+
+const rt: C2 = 'c';
+
+/*
+  NonNullable<Type> - извлекает тип из Type, исключая null и undefined.
+*/
+
+let value: string | null | undefined;
+let nonNullableValue: NonNullable<typeof value>;
+// теперь nonNullableValue это string
+
+/* 
+  Omit<T, K extends keyof T> - выбирает свойства объекта типа T, исключая те, которые указаны в K.
+*/
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+let omittedPerson: Omit<Person, 'age'> = {
+  name: 'Nik',
+};
+// теперь omittedPerson может быть { name: string; }
+
+/*
+
+*/
+
+/* 
+  Pick<T, K extends keyof T> - выбирает свойства объекта типа T с ключами, указанными в K. 
+*/
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = Pick<Todo, 'title' | 'completed'>;
+
+const todo: TodoPreview = {
+  title: 'Room',
+  completed: true,
+};
+
+/* 
+  Readonly<T> - делает все свойства объекта типа T доступными только для чтения. 
 */
 
 interface User {
@@ -19,23 +94,9 @@ const user: Readonly<User> = {
 
 user.name = 'Tom'; /// Cannot assign to 'name' because it is a read-only property.
 
-/* ! 
-     Required<U> - все поля делает обязательными к заполнению.
- */
-
-interface UserData {
-  phone?: number;
-  addres?: string;
-}
-
-const userData: Required<UserData> = {
-  phone: 132,
-  addres: 'Street',
-};
-
-/* ! 
-     Record<Key, Value> - создает тип, который является записью с ключами, 
-     определенными в первом параметре, и значениями типа, определенного во втором параметре.
+/*  
+  Record<Key, Value> - создает тип, который является записью с ключами, 
+  определенными в первом параметре, и значениями типа, определенного во втором параметре.
 */
 
 interface PageInfo {
@@ -50,19 +111,16 @@ const example: Record<Route, PageInfo> = {
   contact: { title: 'contact' },
 };
 
-/* ! 
-     Pick<T, K extends keyof T> - выбирает свойства объекта типа T с ключами, указанными в K. 
-*/
+/* 
+  Required<U> - все поля делает обязательными к заполнению.
+ */
 
-interface Todo {
-  title: string;
-  description: string;
-  completed: boolean;
+interface UserData {
+  phone?: number;
+  addres?: string;
 }
 
-type TodoPreview = Pick<Todo, 'title' | 'completed'>;
-
-const todo: TodoPreview = {
-  title: 'Room',
-  completed: true,
+const userData: Required<UserData> = {
+  phone: 132,
+  addres: 'Street',
 };
